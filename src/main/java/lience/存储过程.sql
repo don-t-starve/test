4c332b0cda883b73b348b7ab6c9c87dd3f3c31df
wangyong-java <1376623484@qq.com>
2020-02-20 14:02:16 +0800

    存储过程

---
+++ src/main/java/lience/存储过程.sql
@@ -0,0 +1,29 @@
+DECLARE
+  TYPE SCORE_LIST IS RECORD(
+       ID_SCORE NUMBER,
+       MANUAL_SCORE NUMBER, 
+       MANUAL_SCORE_REASON VARCHAR2(4000), 
+       SCORE_RULE_DEF_ID NUMBER
+);
+  TYPE SCORE_LIST_TAB IS TABLE OF SCORE_LIST;  
+  CURSOR cur_score IS SELECT * FROM score_INS t WHERE t.MANUAL_SCORE_REASON = 'AA';--SELECT语句换成前手最后一岗打分ins
+  v_score cur_score%ROWTYPE;
+  V_LIST SCORE_LIST_TAB;
+  CNT INT;
+BEGIN
+  SELECT I.ID_SCORE, I.MANUAL_SCORE, I.MANUAL_SCORE_REASON, I.SCORE_RULE_DEF_ID bulk collect INTO V_LIST 
+  FROM SCORE_INS I WHERE I.MANUAL_SCORE_REASON = 'BB';--select语句换成要被修改的那一套ins
+  CNT := V_LIST.COUNT;
+  OPEN cur_score;
+  LOOP 
+     FETCH cur_score INTO v_score;
+     EXIT WHEN cur_score%NOTFOUND;
+          FOR I IN 1 .. CNT LOOP
+              IF v_score.SCORE_RULE_DEF_ID = V_LIST(I).SCORE_RULE_DEF_ID THEN--两个集合循环def相同的才更新
+                UPDATE SCORE_INS SET MANUAL_SCORE = v_score.MANUAL_SCORE WHERE ID_SCORE = V_LIST(I).ID_SCORE;
+                commit;
+              END IF; 
+          END LOOP;
+   END LOOP;
+   CLOSE cur_score;
+END;
--- src/main/java/util/ScoreUtil.java
+++ src/main/java/util/ScoreUtil.java
@@ -1,10 +1,24 @@
 package util;
 
-import java.awt.*;
+import bean.TagRuleDef;
+import bean.TagRuleInstance;
+
+import java.awt.GridBagConstraints;
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 public class ScoreUtil {
     public static GridBagConstraints buildConstraints(int gridX, int gridY, int gridHeight,
-            int gridWidth, int weightX, int weightY, int fill) {
+                                                      int gridWidth, int weightX, int weightY, int fill) {
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.gridx = gridX;
         constraints.gridy = gridY;
@@ -19,5 +33,15 @@ public class ScoreUtil {
     public static String getCurrentPath() {
         String path = ScoreUtil.class.getResource("/").getFile().toString();
         return path.substring(0, path.indexOf("DataModify"));
+    }
+
+    public static TagRuleInstance buildTree(List<TagRuleInstance> instanceList) {
+        Map<Long, List<TagRuleInstance>> childMapGroup = instanceList.stream().collect(Collectors.groupingBy(TagRuleInstance::getParentInstanceId));
+        System.out.println(childMapGroup.toString());
+        return null;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
